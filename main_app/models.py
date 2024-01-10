ea8b8d9d85d6a6bd05eabd8a1f3561b6c161d253
smackeem <smackeem@outlook.com>
2024-01-10 11:05:06 -0500

    Rename model from Shows to Show and use python interactive shell to create some Shows in my database.

--- main_app/models.py
+++ main_app/models.py
@@ -1,9 +1,9 @@
 from django.db import models
 
 # Create your models here.
-class Shows(models.Model):
+class Show(models.Model):
     title = models.CharField(max_length = 100)
     network = models.CharField(max_length = 10)
     run = models.CharField(max_length = 20)
     genre = models.CharField(max_length = 100)
-    season = models.IntegerField(min=1)
+    season = models.IntegerField()
--- main_app/views.py
+++ main_app/views.py
@@ -2,22 +2,22 @@ from django.shortcuts import render
 
 # Create your views here.
 
-shows = [
-    {'title': 'Game of Thrones', 'network': 'HBO', 'run': '2011-19', 'genre': 'Drama', 'seasons': 8},
-    {'title': "Grey's Anatomy", 'network': 'ABC', 'run': '2005-present', 'genre': 'Drama', 'seasons': 20},
-    {'title': 'Scandal', 'network': 'ABC', 'run': '2012-18', 'genre': 'Drama', 'seasons': 7},
-    {'title': 'How To Get Away With Murder', 'network': 'ABC', 'run': '2014-20', 'genre': 'Drama', 'seasons': 6},
-    {'title': 'Billions', 'network': 'SHOWTIME', 'run': '2016-2023', 'genre': 'Drama', 'seasons': 7},
-    {'title': 'True Blood', 'network': 'HBO', 'run': '2008-14', 'genre': 'Drama', 'seasons': 7},
-    {'title': 'The Big Bang Theory', 'network': 'CBS', 'run': '2007-19', 'genre': 'Sitcom', 'seasons': 12},
-    {'title': 'Insecure', 'network': 'HBO', 'run': '2016-21', 'genre': 'Drama', 'seasons': 5},
-    {'title': 'Watchmen', 'network': 'HBO', 'run': '2019', 'genre': 'Drama', 'seasons': 1},
-    {'title': 'Succession', 'network': 'HBO', 'run': '2018-present', 'genre': 'Drama', 'seasons': 4},
-    {'title': 'Mad Men', 'network': 'AMC', 'run': '2007-15', 'genre': 'Drama', 'seasons': 7},
-    {'title': 'Breaking Bad', 'network': 'AMC', 'run': '2008-13', 'genre': 'Drama', 'seasons': 5},
-    {'title': 'The Good Fight', 'network': 'CBS', 'run': '2017-present', 'genre': 'Drama', 'seasons': 6},
-    {'title': 'The Crown', 'network': 'NETFLIX', 'run': '2016-present', 'genre': 'Drama', 'seasons': 6},
-]
+# shows = [
+#     {title='Game of Thrones', network='HBO', run='2011-19', genre='Drama', seasons=8},
+#    {title="Grey's Anatomy", network='ABC', run='2005-present', genre='Drama', seasons=20},
+#     {title='Scandal', network='ABC', run='2012-18', genre='Drama', seasons=7},
+#     {title='How To Get Away With Murder', network='ABC', run='2014-20', genre='Drama', seasons=6},
+#     {title='Billions', network='SHOWTIME', run='2016-2023', genre='Drama', seasons=7},
+#     {title='True Blood', network='HBO', run='2008-14', genre='Drama', seasons=7},
+#     {title='The Big Bang Theory', network='CBS', run='2007-19', genre='Sitcom', seasons=12},
+#     {title='Insecure', network='HBO', run='2016-21', genre='Drama', seasons=5},
+#     {title='Watchmen', network='HBO', run='2019', genre='Drama', seasons=1},
+#     {title='Succession', network='HBO', run='2018-present', genre='Drama', seasons=4},
+#     {title='Mad Men', network='AMC', run='2007-15', genre='Drama', seasons=7},
+#     {title='Breaking Bad', network='AMC', run='2008-13', genre='Drama', seasons=5},
+#     {title='The Good Fight', network='CBS', run='2017-present', genre='Drama', seasons=6},
+#     {title='The Crown', network='NETFLIX', run='2016-present', genre='Drama', seasons=6},
+# ]
 
 def home(request):
     return render(request, 'home.html')
